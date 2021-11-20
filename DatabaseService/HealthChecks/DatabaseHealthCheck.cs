a692f68486f2d51d57ee4b4e2d147451c736967c
burningbridges9 <talipov9@gmail.com>
2021-11-20 14:14:01 +0300

    Add project files.

---
+++ DatabaseService/HealthChecks/DatabaseHealthCheck.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DatabaseService.HealthChecks
+{
+    public class DatabaseHealthCheck : IHealthCheck
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var healthy = await IsDbConnectionOkAsync(); 
+            return healthy
+                ? HealthCheckResult.Healthy("Database connection is OK")
+                : HealthCheckResult.Unhealthy("Database connection ERROR");
+        }
+
+        private Task<bool> IsDbConnectionOkAsync()
+        {
+            return Task.FromResult(DateTime.Now.Millisecond % 2 == 0);
+        }
+    }
+}
